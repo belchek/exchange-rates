adfb4e9a8d7a28014aa268965e13f59c06f2ff1a
belchek <belousov925@mail.ru>
2021-08-03 16:32:39 +0300

    exchange rate

--- js/app.js
+++ js/app.js
@@ -1,29 +1,45 @@
 'use strict';
 
-function updateExchangeRates(update) {
-    const date = document.getElementById('date');
-    date.textContent = update.date;
-    const usdBuyRate = document.getElementById('usd-buy-rate');
-    usdBuyRate.textContent = update.usd.buyRate;
-    const usdSellRate = document.getElementById('usd-sell-rate');
-    usdSellRate.textContent = update.usd.sellRate;
-    const eurBuyRate = document.getElementById('eur-buy-rate');
-    eurBuyRate.textContent = update.eur.buyRate;
-    const eurSellRate = document.getElementById('eur-sell-rate');
-    eurSellRate.textContent = update.eur.sellRate;
-
+function exchange (date, usd, eur) {
+    this.date = date;
+    this.usd = usd;
+    this.eur = eur;
 }
 
-const update = {
-    date: '23 июля',
-    usd: {
-        buyRate: '74,75',
-        sellRate: '76,24',
-    },
-    eur: {
-        buyRate: '87,70',
-        sellRate: '89,74',
-    },
+function updateExchangeRates(exchange) {
+    const date = document.getElementById('date');
+    date.textContent = exchange.date;
+    const usdBuyRate = document.getElementById('usd-buy-rate');
+    usdBuyRate.textContent = exchange.usd.buyRate;
+    const usdSellRate = document.getElementById('usd-sell-rate');
+    usdSellRate.textContent = exchange.usd.sellRate;
+    const eurBuyRate = document.getElementById('eur-buy-rate');
+    eurBuyRate.textContent = exchange.eur.buyRate;
+    const eurSellRate = document.getElementById('eur-sell-rate');
+    eurSellRate.textContent = exchange.eur.sellRate;
+}
 
-};
+function handleSubmit() {
+
+    console.log(exchange);
+    return exchange;
+}
+
+function handleSuccess(evt) {
+    if (evt.target.status !== 200) {
+        // TODO: handle error
+        return;
+    }
+
+    const update = JSON.parse(evt.target.responseText);
+    // TODO: work with data
+    return update;
+}
+const request = new XMLHttpRequest();
+request.open('GET', 'http://localhost:9999/api/hw13');
+request.responseType = 'json';
+request.onload = handleSuccess;
+request.send();
+console.log(request.response);
+const update = new exchange();
 updateExchangeRates(update);
